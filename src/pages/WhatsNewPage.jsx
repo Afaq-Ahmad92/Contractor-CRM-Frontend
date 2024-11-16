5acdb08b1966a9c379733b93976940fa18eb33d4
Muhammad Hussain <7phussain@gmail.com>
2024-11-17 00:11:26 +0500

    Delete src directory

--- src/pages/WhatsNewPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const WhatsNewPage = () => {
-  return (
-    <div>WhatsNewPage</div>
-  )
-}
-
-export default WhatsNewPage
--- src/pages/WorkflowsPage.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const WorkflowsPage = () => {
-  return (
-    <div>WorkflowsPage</div>
-  )
-}
-
-export default WorkflowsPage
