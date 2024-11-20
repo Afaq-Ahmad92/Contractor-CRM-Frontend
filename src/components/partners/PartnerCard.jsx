a48b8926ca082f3c6b5df79cd4ea8752c7c2f437
Muhammad Hussain <7phussain@gmail.com>
2024-11-20 20:30:43 +0500

    fixed

--- src/components/partners/PartnerCard.jsx
+++ src/components/partners/PartnerCard.jsx
@@ -21,6 +21,7 @@ import DraftsIcon from "@mui/icons-material/Drafts";
 import SendIcon from "@mui/icons-material/Send";
 import ShareIcon from "@mui/icons-material/Share";
 import { usePartnersContext } from "./partnersContext";
+import { FaRegCircleCheck } from "react-icons/fa6";
 const PartnerCard = ({
   name,
   email,
@@ -30,7 +31,7 @@ const PartnerCard = ({
   rating,
   statusIcon,
 }) => {
-  const {view} = usePartnersContext();
+  const { view } = usePartnersContext();
   const [menu, setMenu] = useState(false);
 
   const toggleMenu = () => {
@@ -59,76 +60,84 @@ const PartnerCard = ({
       <input
         type="checkbox"
         className=" w-[15px] h-[15px] group-hover:block hidden absolute top-3 left-3"
-       />
+      />
       {/* Options Menu */}
       {menu && (
-  <div
-    className="absolute bg-white border rounded-lg shadow-lg w-72 z-[9999] left-3 max-h-80 overflow-y-auto"
-  >
-    <ul className="py-2 text-sm text-gray-700">
-      <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
-        <StarIcon className="text-gray-500 mr-2" /> Add Review
-      </li>
-      <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
-        <FavoriteIcon className="text-gray-500 mr-2" /> Add to Favorites
-      </li>
-      <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
-        <GroupIcon className="text-gray-500 mr-2" /> Add to Group
-      </li>
-      <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
-        <ShieldIcon className="text-gray-500 mr-2" /> Add to Insurance Request
-      </li>
-      <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
-        <ListIcon className="text-gray-500 mr-2" /> Add to List
-      </li>
-      <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
-        <ArchiveIcon className="text-gray-500 mr-2" /> Archive
-      </li>
-      <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
-        <DescriptionIcon className="text-gray-500 mr-2" /> Create Contract
-      </li>
-      <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
-        <EditIcon className="text-gray-500 mr-2" /> Edit Profile Information
-      </li>
-      <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
-        <SyncAltIcon className="text-gray-500 mr-2" /> Manage Workflows
-      </li>
-      <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
-        <DeleteIcon className="text-gray-500 mr-2" /> Remove
-      </li>
-      <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
-        <FeedbackIcon className="text-gray-500 mr-2" /> Request Feedback
-      </li>
-      <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
-        <ReplayIcon className="text-gray-500 mr-2" /> Resend Invitations
-      </li>
-      <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
-        <PreviewIcon className="text-gray-500 mr-2" /> Review Onboarding
-      </li>
-      <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
-        <AddTaskIcon className="text-gray-500 mr-2" /> Send Agreement for Signing
-      </li>
-      <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
-        <DraftsIcon className="text-gray-500 mr-2" /> Send Email Login Link
-      </li>
-      <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
-        <SendIcon className="text-gray-500 mr-2" /> Send Message
-      </li>
-      <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
-        <ShareIcon className="text-gray-500 mr-2" /> Share Profile
-      </li>
-    </ul>
-  </div>
-)}
-
+        <div className="absolute bg-white border rounded-lg shadow-lg w-72 z-[9999] left-3 max-h-80 overflow-y-auto">
+          <ul className="py-2 text-sm text-gray-700">
+            <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <StarIcon className="text-gray-500 mr-2" /> Add Review
+            </li>
+            <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <FavoriteIcon className="text-gray-500 mr-2" /> Add to Favorites
+            </li>
+            <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <GroupIcon className="text-gray-500 mr-2" /> Add to Group
+            </li>
+            <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <ShieldIcon className="text-gray-500 mr-2" /> Add to Insurance
+              Request
+            </li>
+            <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <ListIcon className="text-gray-500 mr-2" /> Add to List
+            </li>
+            <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <ArchiveIcon className="text-gray-500 mr-2" /> Archive
+            </li>
+            <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <DescriptionIcon className="text-gray-500 mr-2" /> Create Contract
+            </li>
+            <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <EditIcon className="text-gray-500 mr-2" /> Edit Profile
+              Information
+            </li>
+            <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <SyncAltIcon className="text-gray-500 mr-2" /> Manage Workflows
+            </li>
+            <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <DeleteIcon className="text-gray-500 mr-2" /> Remove
+            </li>
+            <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <FeedbackIcon className="text-gray-500 mr-2" /> Request Feedback
+            </li>
+            <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <ReplayIcon className="text-gray-500 mr-2" /> Resend Invitations
+            </li>
+            <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <PreviewIcon className="text-gray-500 mr-2" /> Review Onboarding
+            </li>
+            <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <AddTaskIcon className="text-gray-500 mr-2" /> Send Agreement for
+              Signing
+            </li>
+            <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <DraftsIcon className="text-gray-500 mr-2" /> Send Email Login
+              Link
+            </li>
+            <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <SendIcon className="text-gray-500 mr-2" /> Send Message
+            </li>
+            <li className="flex items-center px-4 py-2 hover:bg-gray-100 cursor-pointer">
+              <ShareIcon className="text-gray-500 mr-2" /> Share Profile
+            </li>
+          </ul>
+        </div>
+      )}
 
       {/* Partner Details */}
       <div className="mt-[20px]  flex flex-col gap-[4px] pt-[7px] w-[99%] items-center h-[230px] mx py-2 border-b-[0.5px]">
-        <img
-          src={"/profile.avif"}
-          alt={name}
-          className="w-24 h-24 rounded-[50%] object-cover"
-        />
+        <div className="relative">
+          <img
+            src={"/profile.avif"}
+            alt={name}
+            className="w-24 h-24 rounded-[50%] object-cover"
+          />
+          <span
+            className={`absolute bottom-0 right-0 ${rating > 4.6 ? "bg-green-500" : "bg-yellow-500"} text-white rounded-full`}
+          >
+            <FaRegCircleCheck size={22} />
+          </span>
+        </div>
         <h2 className="text-lg text-blue-500 font-medium">{name}</h2>
         {rating && (
           <div className="flex items-center space-x-1 text-yellow-500">
@@ -160,5 +169,3 @@ const PartnerCard = ({
 };
 
 export default PartnerCard;
-
-
